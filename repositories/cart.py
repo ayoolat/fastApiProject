4e2fc1e351ae5a96a1c02e8926165b1ff68b616d
ayoolat <ayoola_toluwanimi@yahoo.com>
2023-01-05 06:11:20 +0100

    Completed cart repository

---
+++ repositories/cart.py
@@ -0,0 +1,55 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from schema.cart import Cart
+
+
+async def add_to_cart(payload: Cart, db: Session):
+    new_cart = Cart(
+        cake_id=payload.cake_id,
+        user_profile_id=payload.user_profile_id,
+        paid=False,
+        delivered=payload.delivered,
+        quantity=payload.quantity
+    )
+    db.add(new_cart)
+    db.commit()
+    db.refresh(new_cart)
+    return new_cart
+
+
+async def get_current_cart(db: Session, skip: int, limit: int, user_profile_id: int):
+    cakes = db.query(Cart) \
+        .filter(Cart.paid == False, Cart.user_profile_id == user_profile_id) \
+        .offset(limit * (skip - 1)) \
+        .limit(limit)\
+        .all()
+    return cakes
+
+
+async def clear_cart(db: Session, user_profile_id: int):
+    carts = db.query(Cart).filter(Cart.user_profile_id == user_profile_id).all()
+    for cart in carts:
+        db.delete(cart)
+    db.commit()
+    return True
+
+
+async def mark_as_paid(db: Session, user_profile_id: int):
+    carts = db.query(Cart).filter(Cart.user_profile_id == user_profile_id).all()
+    for cart in carts:
+        await cart_update(db, cart)
+    db.commit()
+    return True
+
+
+async def cart_update(db: Session, payload: Cart):
+    cart = await db.query(Cart).filter(Cart.id == payload.id).first()
+    cart_data = payload.dict(exclude_unset=True)
+    for key, value in cart_data.items():
+        setattr(cart, key, value)
+
+    db.add(cart)
+    db.commit()
+    db.refresh(cart)
+    return cart_data
